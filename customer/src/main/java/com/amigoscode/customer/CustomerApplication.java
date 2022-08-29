ac5d1f1fff74c55d661312ab013d5afb8c4a1ca3
chanet93 <ycdiaz1993@gmail.com>
2022-08-29 10:00:18 -0700

    Refactoring microservices for K8s

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages =
         {"com.amigoscode.customer",
          "com.amigoscode.amqp"})
 @EnableEurekaClient
 @EnableFeignClients(basePackages= "com.amigoscode.clients")
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
 
     public static void main(String[] args) {
--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -6,11 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages =
         {"com.amigoscode.notification",
          "com.amigoscode.amqp"})
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
 
